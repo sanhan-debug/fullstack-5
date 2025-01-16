5df458e9a35629ff12211e24e6fff4c519bf712c
ClassTime <classtime@code.local>
2025-01-16 17:13:18 +0400

    uri

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv'
 import { productRouter } from './Routes/productRouter.js'
 import { connect } from 'mongoose'
 import cors from 'cors'
-
+// mongodb+srv://sanhan:sanhan2006@products5.5wcna.mongodb.net/?retryWrites=true&w=majority&appName=products5
 
 dotenv.config()
 const app = express()
